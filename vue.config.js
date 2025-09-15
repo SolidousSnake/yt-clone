408c8cb5a23d1338ae278f23fac0861f23f33032
Timour <67632745+SolidousSnake@users.noreply.github.com>
2025-09-15 22:32:52 +0300

    Update(config)

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,13 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/yt-clone/'
+      : '/',
+
+  devServer: {
+    historyApiFallback: true
+  }
 })
